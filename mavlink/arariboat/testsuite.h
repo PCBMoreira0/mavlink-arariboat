ae06ad40386cabcd6113644838a3cc818847f780
adrianowerneck <adrianowerneck@id.uff.br>
2023-08-05 01:17:26 -0300

    Added lora params

--- mavlink/arariboat/testsuite.h
+++ mavlink/arariboat/testsuite.h
@@ -396,6 +396,68 @@ static void mavlink_test_aux_system(uint8_t system_id, uint8_t component_id, mav
 #endif
 }
 
+static void mavlink_test_lora_params(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_LORA_PARAMS >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_lora_params_t packet_in = {
+        963497464,17,84,151
+    };
+    mavlink_lora_params_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.bandwidth = packet_in.bandwidth;
+        packet1.spreading_factor = packet_in.spreading_factor;
+        packet1.coding_rate = packet_in.coding_rate;
+        packet1.crc = packet_in.crc;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_LORA_PARAMS_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_LORA_PARAMS_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_lora_params_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_lora_params_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_lora_params_pack(system_id, component_id, &msg , packet1.bandwidth , packet1.spreading_factor , packet1.coding_rate , packet1.crc );
+    mavlink_msg_lora_params_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_lora_params_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.bandwidth , packet1.spreading_factor , packet1.coding_rate , packet1.crc );
+    mavlink_msg_lora_params_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_lora_params_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_lora_params_send(MAVLINK_COMM_1 , packet1.bandwidth , packet1.spreading_factor , packet1.coding_rate , packet1.crc );
+    mavlink_msg_lora_params_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("LORA_PARAMS") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_LORA_PARAMS) != NULL);
+#endif
+}
+
 static void mavlink_test_arariboat(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_heartbeat(system_id, component_id, last_msg);
@@ -404,6 +466,7 @@ static void mavlink_test_arariboat(uint8_t system_id, uint8_t component_id, mavl
     mavlink_test_temperatures(system_id, component_id, last_msg);
     mavlink_test_gps_info(system_id, component_id, last_msg);
     mavlink_test_aux_system(system_id, component_id, last_msg);
+    mavlink_test_lora_params(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
